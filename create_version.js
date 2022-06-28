f4334c28549e79c93983173f545166c6c013d0c0
Devon Harris <devon_harris@glic.com>
2022-06-28 17:59:03 -0400

    This should be a good place to start for uploading bianries and all constituent parts to TF Cloud.

---
+++ create_version.js
@@ -0,0 +1,39 @@
+const fetch = require("node-fetch");
+const Headers = require("node-fetch");
+
+let CreateVersion = async function (orgName, providerName, version, keyID) {
+  
+  let versionURL = `https://app.terraform.io/api/v2/organizations/${orgName}/registry-providers/private/${orgName}/${providerName}/versions`;
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/vnd.api+json");
+  myHeaders.append(
+    "Authorization",
+    process.env.TF_CLOUD_BEARER_TOKEN
+  );
+
+  var raw = JSON.stringify({
+    data: {
+      type: "registry-provider-versions",
+      attributes: {
+        version: `${version}`,
+        "key-id": `${keyID}`,
+        protocols: ["5.0"],
+      },
+    },
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  return new Promise (fetch(versionURL, requestOptions)
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error)));
+}
+
+module.exports(CreateVersion);
--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
 
+const CreateVersion = require("./create_version");
+const UploadBinaries = require("./upload_binaries");
+const UploadChecksums = require("./upload_checksums");
 
-// most @actions toolkit packages have async methods
-async function run() {
+async function runUpload() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const keyID = core.getInput("key_id");
+    const version = core.getInput("version");
+    const binaryLocation = core.getInput("binary_location");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const orgName = "gd-test";
+    const providerName = "gd-stripe-provider";
 
-    core.setOutput('time', new Date().toTimeString());
+    // create a provider version  on registry
+
+    let checksumResponse = JSON.parse(await CreateVersion(orgName, providerName, version, keyID)); //returns checksum links to upload checksums to
+
+    // upload checksums using links from provider version response
+
+    let shaSumsLinks = {shaSum: checksumResponse['data']['links']['shasum-upload'], shaSumSig: checksumResponse['data']['links']['shasums-sig-upload']};
+    await UploadChecksums(binaryLocation, shaSumsLinks);
+    
+    //create all providers for the version created above, as per the artifacts in the provided directory
+
+    await UploadBinaries(orgName, providerName, version, binaryLocation);
+    
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run();
+runUpload();
